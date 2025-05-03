480392bdc85918fb12fc6ba7927233999c5c17fe
Vijay Hardaha <vijayhardaha@gmail.com>
2025-05-03 11:59:43 +0530

    feat: add unit tests for clipboard utility functions and platform detection

---
+++ src/bin/util/getCopyCommand.test.ts
@@ -0,0 +1,19 @@
+import { SUPPORTED_PLATFORMS } from "../../constants/platforms";
+import { COPY } from "../constants/commands";
+
+import { getCopyCommand } from ".";
+
+describe("getCopyCommand", () => {
+  test("Should return the copy command for Mac OSX", () => {
+    expect(getCopyCommand(SUPPORTED_PLATFORMS.DARWIN)).toEqual(COPY.DARWIN);
+  });
+
+  test("Should return the copy command for Linux", () => {
+    expect(getCopyCommand(SUPPORTED_PLATFORMS.LINUX)).toEqual(COPY.LINUX);
+    expect(getCopyCommand()).toEqual(COPY.LINUX);
+  });
+
+  test("Should return the copy command for Windows", () => {
+    expect(getCopyCommand(SUPPORTED_PLATFORMS.WIN32)).toEqual(COPY.WIN32);
+  });
+});
---
+++ src/bin/util/isSupportedPlatform.test.ts
@@ -0,0 +1,15 @@
+import { SUPPORTED_PLATFORMS } from "../../constants/platforms";
+
+import { isSupportedPlatform } from ".";
+
+describe("isSupportedPlatform", () => {
+  test("Should return true if the platform is supported", () => {
+    Object.values(SUPPORTED_PLATFORMS).forEach((platform) => {
+      expect(isSupportedPlatform(platform)).toEqual(true);
+    });
+  });
+
+  test("Should return false if the platform is unsupported", () => {
+    expect(isSupportedPlatform("os2")).toEqual(false);
+  });
+});
