fb1ab710374c0492c8e8020cb3b53357fe1fd0a7
Vijay Hardaha <vijayhardaha@proton.me>
2026-03-02 17:39:08 +0530

    chore: format with standards

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,17 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  build: {
-    sourcemap: true,
-    emptyOutDir: true,
-    lib: {
-      entry: "src/index.ts",
-      name: "vegan-ipsum",
-      formats: ["es", "cjs"],
-      fileName: (format) => (format === "es" ? "index.mjs" : "index.cjs"),
-    },
-    rollupOptions: {
-      output: {
-        exports: "named",
-      },
-    },
-    outDir: "dist",
-  },
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-      outDir: "types",
-    }),
-  ],
+	build: {
+		sourcemap: true,
+		emptyOutDir: true,
+		lib: {
+			entry: "src/index.ts",
+			name: "vegan-ipsum",
+			formats: ["es", "cjs"],
+			fileName: (format) => (format === "es" ? "index.mjs" : "index.cjs"),
+		},
+		rollupOptions: { output: { exports: "named" } },
+		outDir: "dist",
+	},
+	plugins: [dts({ insertTypesEntry: true, outDir: "types" })],
 });
