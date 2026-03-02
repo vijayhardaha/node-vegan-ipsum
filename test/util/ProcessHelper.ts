3ebdd4b8e001bfba3971d0b4e9719c5483ad1d0f
Vijay Hardaha <vijayhardaha@proton.me>
2026-03-02 16:06:27 +0530

    test: format file with new standards

--- test/util/ProcessHelper.ts
+++ test/util/ProcessHelper.ts
@@ -1,20 +1,20 @@
 class ProcessHelper {
-  public cachedPlatform: string;
+	public cachedPlatform: string;
 
-  constructor() {
-    this.cachedPlatform = process.platform;
-  }
+	constructor() {
+		this.cachedPlatform = process.platform;
+	}
 
-  public resetPlatform() {
-    this.setPlatform(this.cachedPlatform);
-  }
+	public resetPlatform() {
+		this.setPlatform(this.cachedPlatform);
+	}
 
-  public setPlatform(platform: string = this.cachedPlatform) {
-    Object.defineProperty(process, "platform", {
-      value: platform,
-      configurable: true, // Ensure the property can be redefined
-    });
-  }
+	public setPlatform(platform: string = this.cachedPlatform) {
+		Object.defineProperty(process, "platform", {
+			value: platform,
+			configurable: true, // Ensure the property can be redefined
+		});
+	}
 }
 
 export default ProcessHelper;
